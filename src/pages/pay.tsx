2a2efe36ce0871882c0fb5ad1a4a4a3c794d43e2
reeshavacharya <reeshav99acharya@gmail.com>
2023-04-12 13:11:53 +0545

    used kuber-client-js, various issues detected

--- src/pages/pay.tsx
+++ src/pages/pay.tsx
@@ -1,36 +1,38 @@
 import { NextPage } from "next"
 import React, { useEffect, useState } from "react";
 import { kuber, txHex } from "./api/kuber";
-import { CardanoWallet, useWallet } from "@martifylabs/mesh-react";
+import { useWallet } from "@martifylabs/mesh-react";
+import {Kuber, CIP30Wallet}  from '../../kuber-client-js/src'
+
 
 const Pay: NextPage = () =>{
-    const { wallet, connected } = useWallet();
     async function pay(ada : number){
-        if(connected){
-            const walletAddress = (await wallet.getUsedAddresses())[0];
-            const jsonBody= {
-                "selections" : [walletAddress],
-                "outputs": [
-                    {
-                      "address": "addr_test1wp34y3rwkf5wgfc32wcwguvlr3wzr9fhj5hw6sk0mul9k5q4pr369",
-                      "value": ada*1000000,
-                      "datum": {"fields": [], "constructor":0}
-                    }
-                  ]
-            }
-            const tx = await kuber(jsonBody)
-            const tx_hex = txHex(tx.tx)
-            try{
-                const tx_signed = await wallet.signTx(tx_hex)
-                await wallet.submitTx(tx_signed);
-                window.alert("transaction submitted successfully!")
-            }
-            catch(error){
-                console.log("tx_hex error:" + error)
-                window.alert("transaction failed: "+ error)
-
-            }
+        const kuber=new Kuber('https://preview.kuberide.com')
+        const providers = CIP30Wallet.listProviders();
+        if(!providers){
+            alert('Wallet Not detected. Install Compatible cip-30 compatible wallet')
+            return
         }
+        let provider=providers[1];
+        const wallet=provider.enable()
+        console.info("Using Browser Wallet",{
+                name: (await wallet).name,
+                balance: (await (await wallet).calculateBalance()).multiAssetsUtf8()
+                })
+        return kuber.buildWithProvider(await wallet,{
+            outputs:[
+                {
+                    "address": "addr_test1wp34y3rwkf5wgfc32wcwguvlr3wzr9fhj5hw6sk0mul9k5q4pr369",
+                    "value": ada*1000000,
+                    "datum": {"fields": [], "constructor":0},
+                    "inlineDatum": true
+                }
+            ]
+        }).then(async tx=>{
+            (await wallet).signAndSubmit(tx)
+        }).catch(e=>{
+            alert((e && e.message) || e)
+        })        
     }
     return(
         <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
@@ -44,7 +46,6 @@ const Pay: NextPage = () =>{
                 }}>pay</button>
             </div>
         </div>
-
     );
 }
 export default Pay
